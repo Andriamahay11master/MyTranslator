b2248ca3c37ca5dd1a75d27e225c695f5021e31a
Mahay <hirimanana@yahoo.fr>
2026-04-07 11:55:13 +0400

    change translation_memory

--- backend/app/services/translation_memory.py
+++ backend/app/services/translation_memory.py
@@ -1,10 +1,10 @@
 translation_memory = {}
 
-def get_from_memory(text, source, target):
+def get_translation(text, source, target):
     key = f"{source}-{target}-{text}"
     return translation_memory.get(key)
 
 
-def save_to_memory(text, source, target, translation):
+def save_translation(text, source, target, translation):
     key = f"{source}-{target}-{text}"
     translation_memory[key] = translation
